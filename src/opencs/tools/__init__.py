6350c1a6f7a9eea69f997e8711f2660704dd2f71
star <sunxing2017@gmail.com>
2026-05-03 08:07:18 +0800

    chore: add tools module __init__ exports

---
+++ src/opencs/tools/__init__.py
@@ -0,0 +1,6 @@
+from opencs.tools.api_tool import APITool
+from opencs.tools.executor import ToolExecutor
+from opencs.tools.protocol import Tool, ToolDescription, ToolResult
+from opencs.tools.registry import ToolRegistry
+
+__all__ = ["Tool", "ToolDescription", "ToolResult", "ToolRegistry", "APITool", "ToolExecutor"]
